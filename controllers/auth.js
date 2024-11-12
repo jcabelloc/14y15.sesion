adbd3217843dc3db58e6fd800cd61af93adde13f
Juan Cabello <jcabelloc@itana.pe>
2024-11-11 20:33:59 -0500

    Autentication y Entrega de JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const Usuario = require('../models/usuario')
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 
 const nodemailer = require('nodemailer');
@@ -48,6 +49,10 @@ exports.postIngresar = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
+    const error = new Error(errors.array()[0].msg)
+    error.statusCode = 422; //400
+    throw error;
+    /*
     return res.status(422).render('auth/ingresar', {
       path: '/ingresar',
       titulo: 'Ingresar',
@@ -57,13 +62,16 @@ exports.postIngresar = (req, res, next) => {
         password: password
       },
       erroresValidacion: errors.array()
-    });
+    });*/
   }
-
 
   Usuario.findOne({ email: email })
     .then(usuario => {
       if (!usuario) {
+        const error = new Error('No se ha encontrado el usuario')
+        error.statusCode = 401; //400
+        throw error;
+        /*
         return res.status(422).render('auth/ingresar', {
           path: '/ingresar',
           titulo: 'Ingresar',
@@ -73,25 +81,34 @@ exports.postIngresar = (req, res, next) => {
             password: password
           },
           erroresValidacion: []
-        });
+        });*/
       }
       bcrypt.compare(password, usuario.password)
         .then(hayCoincidencia => {
-          if (hayCoincidencia) {
-            req.session.autenticado = true;
-            req.session.usuario = usuario;
-            return req.session.save(err => {
-              console.log(err);
-              res.redirect('/')
-            })
+          if (!hayCoincidencia) {
+            const error = new Error('LAs credenciales son invalidas')
+            error.statusCode = 401; //400
+            throw error;
           }
-          req.flash('error', 'Las credenciales son invalidas')
-          res.redirect('/ingresar');
+          const token = jwt.sign(
+            {
+              email: usuario.email,
+              idUsuario: usuario._id.toString()
+            },
+            'Aquievaelpasswordmuymuymuymuysecreto', // Esto debe estar en BD, FS o Servicios Cloud tipo Wallet
+            { expiresIn: '2h' }
+          );
+          res.status(200).json({
+            token: token,
+            idUsuario: usuario._id.toString(),
+            mensaje: 'Token generado con exito'
+          })
         })
         .catch(err => {
-          const error = new Error(err);
-          error.httpStatusCode = 500;
-          return next(error);
+          if (!err.statusCode) {
+            err.statusCode = 500;
+          }
+          next(err)
         });
     })
 };
